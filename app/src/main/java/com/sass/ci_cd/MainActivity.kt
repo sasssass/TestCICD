e0089d7ad8e691895c29c13e82d453570fa4cf37
Ali <s,shb.s.ali@gmail.com>
2023-09-05 18:06:17 +0200

    Log.d

--- app/src/main/java/com/sass/ci_cd/MainActivity.kt
+++ app/src/main/java/com/sass/ci_cd/MainActivity.kt
@@ -2,10 +2,12 @@ package com.sass.ci_cd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("HELLO","HELL YEAH")
     }
 }
