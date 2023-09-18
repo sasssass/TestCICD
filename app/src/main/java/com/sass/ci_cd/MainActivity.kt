bf304769a738a15c4e65f974b1d4241f79f66d90
Ali Shobeyri <ali.shobeyri@daresay.co>
2023-09-18 17:40:38 +0200

    add appcenter

--- app/src/main/java/com/sass/ci_cd/MainActivity.kt
+++ app/src/main/java/com/sass/ci_cd/MainActivity.kt
@@ -2,10 +2,16 @@ package com.sass.ci_cd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCenter.start(application, "d64be6cc-bcf8-4380-9a8d-e234be4bded9",
+            Analytics::class.java, Crashes::class.java)
     }
 }
