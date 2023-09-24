e97e6c92696119258a2fbab155177694f45a1a1c
Ali <s,shb.s.ali@gmail.com>
2023-09-24 18:08:16 +0200

    gsdfgsdfg

--- app/src/main/java/com/sass/ci_cd/MainActivity.kt
+++ app/src/main/java/com/sass/ci_cd/MainActivity.kt
@@ -2,6 +2,7 @@ package com.sass.ci_cd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
@@ -13,5 +14,6 @@ class MainActivity : AppCompatActivity() {
 
         AppCenter.start(application, "d64be6cc-bcf8-4380-9a8d-e234be4bded9",
             Analytics::class.java, Crashes::class.java)
+        Log.d("HELLO", "HELLO")
     }
 }
